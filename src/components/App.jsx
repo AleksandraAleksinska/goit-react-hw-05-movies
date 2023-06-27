855ffee90f00176e86a7b50e7139d18bfb24206e
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-06-27 11:34:53 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "pages/Home/Home";
+import Movies from "pages/Movies/Movies";
+import MovieDetails from "pages/MovieDetails/MovieDetails";
+import NotFound from "pages/NotFound/NotFound";
+
 export const App = () => {
 
   const KEY = 'dce0b8b37fbd78cdab3203c47fa0e91b'
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+   <div>
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/movies'>Movies</Link>
+    </nav>
+
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/movies' element={<Movies />} />
+      <Route path='/movieDetails' element={<MovieDetails />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+   </div> 
   );
 };
