bf936b622ccab81642ae3c7b4a4c53af532e14a6
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-07-04 20:18:18 +0200

    Update tmdbAPI.js

--- src/services/tmdbAPI.js
+++ src/services/tmdbAPI.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
+axios.defaults.params = {
+      api_key: "dce0b8b37fbd78cdab3203c47fa0e91b"
+    }
 
-  const  api_key ='api_key=dce0b8b37fbd78cdab3203c47fa0e91b';
   const  baseURL = 'https://api.themoviedb.org/3/';
 
 
 export const getTrendingMovies = async() => {
     
-        const response = await axios.get(`${baseURL}trending/movie/day?${api_key}`);
+        const response = await axios.get(`${baseURL}trending/movie/day?`);
         const trendingMovies = response.data.results;       
         return trendingMovies;
     
@@ -15,28 +17,28 @@ export const getTrendingMovies = async() => {
 
 export const getMovieDetails = async(movieId) => {
 
-      const response = await axios.get(`${baseURL}movie/${movieId}?${api_key}`);
+      const response = await axios.get(`${baseURL}movie/${movieId}?`);
       const movieDetails = response.data;
       return movieDetails;
 }
 
 export const getMoviesByQuery = async (query) => {
  
-      const response = await axios.get(`${baseURL}search/movie?query=${query}&${api_key}`);
+      const response = await axios.get(`${baseURL}search/movie?query=${query}&`);
       const movies = response.data.results;
       return movies;
 }
 
 export const getReviews= async(movieId) => {
 
-      const response = await axios.get(`${baseURL}movie/${movieId}/reviews?${api_key}`);
+      const response = await axios.get(`${baseURL}movie/${movieId}/reviews?`);
       const movieReview = response.data.results;
       return movieReview;
 }
 
 export const getMovieCast= async(movieId) => {
 
-      const response = await axios.get(`${baseURL}movie/${movieId}/credits?${api_key}`);
+      const response = await axios.get(`${baseURL}movie/${movieId}/credits?`);
       const movieCast = response.data.cast;
       return movieCast;
 }
